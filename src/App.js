0dd1e3837fd2771a5062f9f1abfe4bb7eb8d9f97
meSailesh <mesailesh05@gmail.com>
2019-08-19 12:07:21 +0545

    Rendering content conditionally

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ const [personsState, setPersons] = useState({
     { name: 'Max', age:28 },
     { name: 'Manu', age:26 },
     { name: 'Sailesh', age:23 }
-  ]
+  ],
+  showPersons: false
 });
-const [otherState, setOtherState] = useState('some other value')
-console.log(personsState, otherState);
+// const [otherState, setOtherState] = useState('some other value')
+// console.log(personsState, otherState);
 
 const switchNameHandler = () => {
   //Check commit
@@ -20,8 +21,22 @@ const switchNameHandler = () => {
       { name: 'Maximilian', age:28 },
       { name: 'Manu', age:28 },
       { name: 'Sailesh', age:23 }
-  ] 
+  ],
+  showPersons : personsState.showPersons 
   });
+}
+
+const togglePersonHandler = () => {
+  const doesShow = personsState.showPersons;
+  setPersons({
+    persons:[
+      { name: 'Max', age:28 },
+      { name: 'Manu', age:28 },
+      { name: 'Sailesh', age:23 }
+  ], 
+    showPersons: !doesShow
+  })
+
 }
 
 const style = {
@@ -38,12 +53,24 @@ const style = {
      <p>This is really working!</p>
      <button
      style = {style}
-     onClick = {switchNameHandler}>Switch Name</button>
-     <Person name = {personsState.persons[0].name} age = {personsState.persons[0].age}/>
-     <Person name = {personsState.persons[1].name} age = {personsState.persons[1].age}>
-       My Hobbies: Racing
-     </Person>
-     <Person name = {personsState.persons[2].name} age = {personsState.persons[2].age}/>
+     onClick = {togglePersonHandler}>Show Names</button>
+     {
+       personsState.showPersons ? 
+        <div>
+          <Person 
+          name = {personsState.persons[0].name} 
+          age = {personsState.persons[0].age}/>
+          <Person 
+          name = {personsState.persons[1].name}
+           age = {personsState.persons[1].age}
+           clicked = {switchNameHandler}>
+            My Hobbies: Racing
+          </Person>
+          <Person 
+          name = {personsState.persons[2].name} 
+          age = {personsState.persons[2].age}/>
+        </div> : null
+     }
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,9 @@ import './Person.css';
 const person = (props) => {
   return(
     <div className="Person">
-       <p>I'am {props.name} and I am {props.age} years old!</p>
+       <p
+       onClick = {props.clicked}
+       >I'am {props.name} and I am {props.age} years old!</p>
        <p>{props.children}</p>
     </div>
   )
